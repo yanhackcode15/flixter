7a72f98e50413b26c1effe8275bf439cdc89cc8c
Yan Hu <iamyanhu@gmail.com>
2015-11-26 18:33:50 +0000

    Added course creation page using simple_form  and show page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-	before_action :authenticate_user!, :only => []
+	before_action :authenticate_user!, :only => [:new, :create, :show]
 	def index
 
 	end
