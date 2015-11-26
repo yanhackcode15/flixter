6257507e7a131fc6c61dbe024df8d3320f2dfe5b
Yan Hu <iamyanhu@gmail.com>
2015-11-26 18:58:51 +0000

    Added form error validation in the course model and redirect for error cases in the course controller

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
+	# validates :title, :description, :cost, presence: true
+	# validates :cost, :numericality =>{:greater_than_or_equal_to => 0}
+  validates :title, :presence => true
+  validates :description, :presence => true
+  validates :cost, :presence => true, :numericality => {:greater_than_or_equal_to => 0}
 end
