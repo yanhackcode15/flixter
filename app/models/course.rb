5d0f029e9333b66e34cd70773637bd95e1cd1d2c
Yan Hu <iamyanhu@gmail.com>
2015-11-29 20:01:36 +0000

    Changed routes so the section is no longer under course, added new lesson creation form, added lessons show under the section on the course page

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
-	has_many :sections
+	has_many :sections 
 	# validates :title, :description, :cost, presence: true
 	# validates :cost, :numericality =>{:greater_than_or_equal_to => 0}
   validates :title, :presence => true
